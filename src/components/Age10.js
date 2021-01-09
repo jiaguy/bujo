5fa13f52b3efccf9fe40d8b7fb9e56f34f9d5e4c
Jiaying <jiayingli123@gmail.com>
2021-01-09 01:10:50 -0500

    Created job component

--- src/components/Age10.js
+++ src/components/Age10.js
@@ -1,10 +1,13 @@
 import React from 'react';
-   
+import Jobs from './Jobs';
+
+
+
 const Age10 = () => {
     return (
        <div>
           <h1>Age 10</h1>
-
+          <Jobs name="Sara" />;
        </div>
     );
 }
